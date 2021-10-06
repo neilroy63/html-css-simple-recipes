949f9f19dac967f4be85b9e71b15cdbfc5aa3b39
Neil <neil.roy63@gmail.com>
2021-10-06 19:07:00 +1100

    feature/added_js_file_function_to_throw_error_if_no_valid_selector

--- js/app.js
+++ js/app.js
@@ -1,19 +1,10 @@
+// function to throw an error if there is no valid css selector
 const getElement = (selector) => {
-  const element = document.querySelector(selector)
-
-  if (element) return element
+  const element = document.querySelector(selector);
+  if (element) return element;
   throw Error(
-    `Please double check your class names, there is no ${selector} class`
-  )
-}
+    `Please double check your class name, there is no ${selector} class`
+  );
+};
 
-const links = getElement('.nav-links')
-const navBtnDOM = getElement('.nav-btn')
-
-navBtnDOM.addEventListener('click', () => {
-  links.classList.toggle('show-links')
-})
-
-const date = getElement('#date')
-const currentYear = new Date().getFullYear()
-date.textContent = currentYear
+const links = getElement(".nav-links");
